e0968d22abf6ee342c695ca679da9c21f33190f8
jeswin9567 <jeswinmathew9567@gmail.com>
2025-04-15 14:34:29 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'zustand'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
