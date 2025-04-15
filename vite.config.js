a6a142e521f8787677ccbe9b425ffb0d64cdb7b1
jeswin9567 <jeswinmathew9567@gmail.com>
2025-04-15 15:04:27 +0530

    new netlify config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'zustand'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
